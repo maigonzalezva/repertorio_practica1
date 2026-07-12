ca232e524e98cf4520a26bb85ccea2175ede4dad
mai.gonzalezva <mai.gonzalezva@duocuc.cl>
2026-07-12 15:32:26 -0400

    Nueva funcion

--- marti.py
+++ marti.py
@@ -1,7 +1,7 @@
 
 
-def sumar(numero1, numero2):
-    suma = numero1+numero2
+def sumar(numero4, numero2):
+    suma = numero4+numero2
     return suma
 
 
@@ -11,4 +11,33 @@ numero1 = int(input("ingrese otro numero: "))
 resultado = sumar(numero3, numero1)
 print(resultado)
 
+def validacion_duracion(duracion):
+    try:
+        duracion = int(duracion)
+        if duracion > 0:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
+    
+duracion = "1000"
+    
+resultado = validacion_duracion(duracion)
+print(resultado)
+
+def validacion_duracion(duracion):
+    try:
+        duracion = int(duracion)
+        if duracion > 0:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
+    
+duracion = "1000"
+    
+resultado = validacion_duracion(duracion)
+print(resultado)
 
