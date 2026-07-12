5bf68725c6bace0d358fdb87df2a3d962171eaaa
mai.gonzalezva <mai.gonzalezva@duocuc.cl>
2026-07-11 22:59:09 -0400

    cambio

--- marti.py
+++ marti.py
@@ -6,9 +6,9 @@ def sumar(numero1, numero2):
 
 
 numero3 = int(input("ingrese un numero: "))
-numero2 = int(input("ingrese otro numero: "))
+numero1 = int(input("ingrese otro numero: "))
 
-resultado = sumar(numero3, numero2)
+resultado = sumar(numero3, numero1)
 print(resultado)
 
 
