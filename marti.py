f0561ef2eebb609d69e61b55c1de212ca6299609
mai.gonzalezva <mai.gonzalezva@duocuc.cl>
2026-07-11 22:46:46 -0400

    nueva carpeta

--- marti.py
+++ marti.py
@@ -1,18 +1,14 @@
-hechizos = {
-'H001': ['Llamarada Solar', 'elemental', 5, 'C', False, 'Ignus el Ardiente'],
-'H002': ['Escudo de Escarcha', 'elemental', 3, 'C', False, 'Dama Fenwick'],
-'H003': ['Rayo Astral', 'arcana', 7, 'R', False, 'Magíster Orin'],
-'H004': ['Cadena de Almas', 'oscura', 9, 'L', True, 'El Innombrable'],
-'H005': ['Portal Menor', 'arcana', 4, 'R', False, 'Selene Valdour'],
-'H006': ['Toque Vampírico', 'oscura', 6, 'R', True, 'Mordath'],
-}
 
-reservas = {
-'H001': [120, 8],
-'H002': [90, 0],
-'H003': [340, 3],
-'H004': [999, 2],
-'H005': [210, 5],
-'H006': [450, 4],
-}
+
+def sumar(numero1, numero2):
+    suma = numero1+numero2
+    return suma
+
+
+numero1 = int(input("ingrese un numero: "))
+numero2 = int(input("ingrese otro numero: "))
+
+resultado = sumar(numero1, numero2)
+print(resultado)
+
 
---
+++ marti2.py
@@ -0,0 +1,14 @@
+def validacion_duracion(duracion):
+    try:
+        duracion = int(duracion)
+        if duracion > 0:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
+    
+duracion = "-10"
+    
+resultado = validacion_duracion(duracion)
+print(resultado)
